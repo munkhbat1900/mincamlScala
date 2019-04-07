3fe5b37a9dd18c00e73ca8330cd90b47ce02b1ce
Avirmed Munkhbat <munkhbataa@gmail.com>
2019-04-07 14:54:02 +0900

    commit

--- src/main/scala/com/munkhbat/scala/mincaml/parser/Visitor.scala
+++ src/main/scala/com/munkhbat/scala/mincaml/parser/Visitor.scala
@@ -1,7 +1,7 @@
 package com.munkhbat.scala.mincaml.parser
 
 import com.munkhbat.scala.mincaml.ast.Ast
-import com.munkhbat.scala.mincaml.ast.Ast.{MBool, MInt}
+import com.munkhbat.scala.mincaml.ast.Ast._
 
 class Visitor extends MincamlBaseVisitor[Ast.Exp] {
   override def visitEmptyParenExp(ctx: MincamlParser.EmptyParenExpContext): Ast.Exp = visitChildren(ctx)
@@ -17,33 +17,60 @@ class Visitor extends MincamlBaseVisitor[Ast.Exp] {
   }
 
   override def visitBoolExp(ctx: MincamlParser.BoolExpContext): Ast.Exp = {
-    println(ctx)
-    MBool(true)
+    MBool(ctx.getText.toBoolean)
   }
 
   override def visitIdentExp(ctx: MincamlParser.IdentExpContext): Ast.Exp = {
-    visitChildren(ctx)
+    MVar(ctx.getText)
   }
 
   override def visitIntExp(ctx: MincamlParser.IntExpContext): Ast.Exp = {
-    println("haha")
-    println(ctx.INT().getSymbol)
-    MInt(50)
+    MInt(ctx.getText.toInt)
   }
 
-  override def visitFloatExp(ctx: MincamlParser.FloatExpContext): Ast.Exp = visitChildren(ctx)
+  override def visitFloatExp(ctx: MincamlParser.FloatExpContext): Ast.Exp = {
+    MFloat(ctx.getText.toFloat)
+  }
 
-  override def visitGetExp(ctx: MincamlParser.GetExpContext): Ast.Exp = visitChildren(ctx)
+  override def visitGetExp(ctx: MincamlParser.GetExpContext): Ast.Exp = {
+    val arr = visit(ctx.simple_exp())
+    val index = visit(ctx.exp())
+    MGet(arr, index)
+  }
 
-  override def visitPlusMinusExp(ctx: MincamlParser.PlusMinusExpContext): Ast.Exp = visitChildren(ctx)
+  override def visitPlusMinusExp(ctx: MincamlParser.PlusMinusExpContext): Ast.Exp = {
+    val left = visit(ctx.left)
+    val right = visit(ctx.right)
+    val operand = ctx.op.getText
+    operand match {
+      case "+" => MAdd(left, right)
+      case "-" => MSub(left, right)
+    }
+  }
 
-  override def visitLetTupleExp(ctx: MincamlParser.LetTupleExpContext): Ast.Exp = visitChildren(ctx)
+  override def visitLetTupleExp(ctx: MincamlParser.LetTupleExpContext): Ast.Exp = {
+    val tuple = ctx.pat()
+    val e = ctx.exp(0)
+    val inExp = ctx.exp(1)
+    // TODO
+    MUnit()
+//    MLetTuple
+  }
 
-  override def visitFNegExp(ctx: MincamlParser.FNegExpContext): Ast.Exp = visitChildren(ctx)
+  override def visitFNegExp(ctx: MincamlParser.FNegExpContext): Ast.Exp = {
+    MFNeg(visit(ctx.exp()))
+  }
 
-  override def visitSimpleExp(ctx: MincamlParser.SimpleExpContext): Ast.Exp = visitChildren(ctx)
+  override def visitSimpleExp(ctx: MincamlParser.SimpleExpContext): Ast.Exp = {
+    visitChildren(ctx)
+  }
 
-  override def visitPutExp(ctx: MincamlParser.PutExpContext): Ast.Exp = visitChildren(ctx)
+  override def visitPutExp(ctx: MincamlParser.PutExpContext): Ast.Exp = {
+    val arrExp = visit(ctx.simple_exp())
+    val indexExp = visit(ctx.exp(0))
+    val valToPut = visit(ctx.exp(1))
+    MPut(arrExp, indexExp, valToPut)
+  }
 
   override def visitNotExp(ctx: MincamlParser.NotExpContext): Ast.Exp = visitChildren(ctx)
 
