786ae2bd51ba877cac629ae1c545c22b3d2d6ac7
Avirmed Munkhbat <munkhbataa@gmail.com>
2019-04-07 12:15:15 +0900

    commit

--- src/main/scala/com/munkhbat/scala/mincaml/Main.scala
+++ src/main/scala/com/munkhbat/scala/mincaml/Main.scala
@@ -1,22 +1,15 @@
 package com.munkhbat.scala.mincaml
 
 import com.munkhbat.scala.mincaml.parser._
-import org.antlr.v4.runtime.{ANTLRInputStream, CommonTokenStream}
 
 import scala.io.Source
 
 object Main {
   def main(args: Array[String]): Unit = {
-//    val src = Source.fromFile("testFiles/sample.ml").mkString
-//    val charStream = new ANTLRInputStream(src)
-//    val lexer = new MincamlLexer(charStream)
-//    val tokens = new CommonTokenStream(lexer)
-//    val parser = new MincamlParser(tokens)
-//
-//    val visitor = new Visitor()
-//    val ast = visitor.visit(parser.exp())
-//
-//    import sext._
-//    println(ast.treeString)
+    val src = Source.fromFile("testFiles/sample.ml").mkString
+    val ast = new VisitorGeneratorWrapper().generateVisitorJavaWrapper(src)
+
+    import sext._
+    println(ast.treeString)
   }
 }
--- src/main/scala/com/munkhbat/scala/mincaml/parser/VisitorGeneratorWrapper.java
+++ src/main/scala/com/munkhbat/scala/mincaml/parser/VisitorGeneratorWrapper.java
@@ -1,18 +1,22 @@
 package com.munkhbat.scala.mincaml.parser;
 
+import com.munkhbat.scala.mincaml.ast.Ast;
 import org.antlr.v4.runtime.ANTLRInputStream;
 import org.antlr.v4.runtime.CharStream;
 import org.antlr.v4.runtime.CommonTokenStream;
 import org.antlr.v4.runtime.TokenStream;
 
-public class Parse {
-    public static void main(String[] args) {
-        CharStream charStream = new ANTLRInputStream("5 + 6");
+/**
+ * I needed this wrapper because in scala I couldn't call visitor.
+ */
+public class VisitorGeneratorWrapper {
+    public Ast.Exp generateVisitorJavaWrapper(String sourceCode) {
+        CharStream charStream = new ANTLRInputStream(sourceCode);
         MincamlLexer lexer = new MincamlLexer(charStream);
         TokenStream tokenStream = new CommonTokenStream(lexer);
         MincamlParser mincamlParser = new MincamlParser(tokenStream);
 
         Visitor visitor = new Visitor();
-        visitor.visit(mincamlParser.exp());
+        return visitor.visit(mincamlParser.exp());
     }
 }
