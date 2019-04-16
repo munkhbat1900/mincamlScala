b72e51393fec09906b990551f5f62a446583e879
Avirmed Munkhbat <munkhbataa@gmail.com>
2019-04-17 01:42:44 +0900

    commit

--- src/main/scala/com/munkhbat/scala/mincaml/Main.scala
+++ src/main/scala/com/munkhbat/scala/mincaml/Main.scala
@@ -1,15 +1,19 @@
 package com.munkhbat.scala.mincaml
 
 import com.munkhbat.scala.mincaml.parser._
+import com.munkhbat.scala.mincaml.`type`.Typing
 
 import scala.io.Source
 
 object Main {
   def main(args: Array[String]): Unit = {
-    val src = Source.fromFile("testFiles/cls-rec.ml").mkString
+    val src = Source.fromFile("testFiles/sample.ml").mkString
     val ast = new VisitorGeneratorWrapper().generateVisitorJavaWrapper(src)
 
     import sext._
+    println(ast.treeString)
+
+    Typing.infer(ast)
     println(ast.treeString)
   }
 }
---
+++ src/main/scala/com/munkhbat/scala/mincaml/type/Typing.scala
@@ -0,0 +1,183 @@
+package com.munkhbat.scala.mincaml.`type`
+
+import com.munkhbat.scala.mincaml.ast.Ast
+import com.munkhbat.scala.mincaml.ast.Ast._
+import com.munkhbat.scala.mincaml.tools.Id
+
+/**
+  * type inference implementation.
+  * there are good explanation of type inference algorithm in following URL.
+  * https://tech.nikkeibp.co.jp/it/article/COLUMN/20070717/277580/?P=2
+  * it's in Japanese. To view content fully you need to become member.
+  */
+object Typing {
+
+  // type environment for external variables.
+  var extenv = Map[Id.T, Type.T]()
+
+  def occur(r1: Option[Type.T], t2: Type.T): Boolean = {
+    t2 match {
+      case Type.Fun(args, ret) => args.exists(occur(r1, _)) || occur(r1, ret)
+      case Type.Tuple(elems) => elems.exists(occur(r1, _))
+      case Type.Array(t) => occur(r1, t)
+      case Type.Var(r2) if (r1 == r2)=> true
+      case Type.Var(None) => false
+      case Type.Var(Some(r2)) => occur(r1, r2)
+      case _ => false
+    }
+  }
+
+
+
+  def g(env: Map[Id.T, Type.T], e: Ast.Exp): Type.T = {
+    def intArith(e1: Ast.Exp, e2: Ast.Exp) = {
+      unify(Type.Int(), g(env, e1))
+      unify(Type.Int(), g(env, e2))
+      Type.Int()
+    }
+
+    def floatArith(e1: Ast.Exp, e2: Ast.Exp) = {
+      unify(Type.Float(), g(env, e1))
+      unify(Type.Float(), g(env, e2))
+      Type.Float()
+    }
+
+    def logicExp(e1: Ast.Exp, e2: Ast.Exp) = {
+      unify(g(env, e1), g(env, e2))
+      Type.Bool()
+    }
+
+    e match {
+      case MUnit() => Type.Unit()
+      case MBool(_) => Type.Bool()
+      case MInt(_) => Type.Int()
+      case MFloat(_) => Type.Float()
+      case MNot(e) => {
+        unify(Type.Bool(), g(env, e))
+        Type.Bool()
+      }
+      case Ast.MNeg(e) => {
+        unify(Type.Int(), g(env, e))
+        Type.Int()
+      }
+      case Ast.MAdd(e1, e2) => intArith(e1, e2)
+      case Ast.MSub(e1, e2) => intArith(e1, e2)
+      case Ast.MFAdd(e1, e2) => floatArith(e1, e2)
+      case Ast.MFSub(e1, e2) => floatArith(e1, e2)
+      case Ast.MFMul(e1, e2) => floatArith(e1, e2)
+      case Ast.MFDiv(e1, e2) => floatArith(e1, e2)
+      case Ast.MEq(e1, e2) => logicExp(e1, e2)
+      case Ast.MLE(e1, e2) => logicExp(e1, e2)
+
+      case Ast.MIf(e1, e2, e3) => {
+        unify(Type.Bool(), g(env, e1))
+        val t2 = g(env, e2)
+        val t3 = g(env, e3)
+        unify(t2, t3)
+        t2
+      }
+      case Ast.MLet((id, t), e2, e3) => {
+        unify(t, g(env, e2))
+        g(env + (id -> t), e3)
+      }
+      case Ast.MVar(x) if (env.contains(x)) => env(x)
+      case Ast.MVar(x) if (extenv.contains(x)) => extenv(x)
+      case Ast.MVar(x) => {
+        println("free variable" + x.toString() + " assumed as external@.")
+        val t = Type.gentyp()
+        extenv = extenv + (x -> t)
+        t
+      }
+      case Ast.MLetRec(Ast.MFunDef(name, args, body), e2) => {
+        val funName = name._1
+        val funTyp = name._2
+        val newEnv = env + (funName -> funTyp)
+        unify(funTyp, Type.Fun(args.map{_._2}, g(newEnv ++ args, body)))
+        g(newEnv, e2)
+      }
+      case Ast.MApp(e1, e2) => {
+        val t = Type.gentyp()
+        val funTyp = g(env, e1)
+        val args = e2.map(g(env, _))
+        unify(funTyp, Type.Fun(args, t))
+        t
+      }
+      case Ast.MTuple(e) => Type.Tuple(e.map(g(env, _)))
+      case Ast.MLetTuple(e1, e2, e3) => {
+        unify(Type.Tuple(e1.map(_._2)), g(env, e2))
+        g(env ++ e1, e3)
+      }
+      case Ast.MArray(e1, e2) => {
+        unify(g(env, e1), Type.Int())
+        Type.Array(g(env, e2))
+      }
+      case Ast.MGet(e1, e2) => {
+        val t = Type.gentyp()
+        unify(Type.Array(t), g(env, e1))
+        unify(Type.Int(), g(env, e2))
+        t
+      }
+      case Ast.MPut(e1, e2, e3) => {
+        val t = g(env, e3)
+        unify(Type.Array(t), g(env, e1))
+        unify(Type.Int(), g(env, e2))
+        Type.Unit()
+      }
+    }
+  }
+
+  /**
+    * make t1 and t2 equal.
+    * @param t1
+    * @param t2
+    */
+  def unify(t1: Type.T, t2: Type.T): Unit = {
+    (t1, t2) match {
+      case (Type.Unit(), Type.Unit()) => ()
+      case (Type.Bool(), Type.Bool()) => ()
+      case (Type.Int(), Type.Int()) => ()
+      case (Type.Float(), Type.Float()) => ()
+      case (Type.Fun(argList1, ret1), Type.Fun(argList2, ret2)) => {
+        if (argList1.length != argList2.length) {
+          throw new Exception("function argument count is not match t1 = " + t1 + " t2 = " + t2)
+        }
+
+        val newList = argList1.zip(argList2)
+        newList.foreach(t => unify(t._1, t._2))
+        unify(ret1, ret2)
+      }
+      case (Type.Tuple(elems1), Type.Tuple(elems2)) => {
+        if (elems1.length != elems2.length) {
+          throw new Exception("tuple element count is not match t1 = " + t1 + " t2 = " + t2)
+        }
+        val newList = elems1.zip(elems2)
+        newList.foreach(t => unify(t._1, t._2))
+      }
+      case (Type.Array(t1), Type.Array(t2)) => {
+        unify(t1, t2)
+      }
+      case (Type.Var(r1), Type.Var(r2)) if (r1 == r2) => ()
+      case (Type.Var(Some(t11)), _) => unify(t11, t2)
+      case (_, Type.Var(Some(t22))) => unify(t1, t22)
+      case (r@Type.Var(r1), _) => {
+        if (occur(r1, t2)) {
+          throw new Exception("type occur t1 = " + t1 + " t2 = " + t2)
+        }
+        r1 match {
+          case None => r.a = Some(t2)
+          case Some(t11) => unify(t11, t2)
+        }
+      }
+      case (_, Type.Var(r2)) => {
+        unify(t2, t1)
+      }
+      case (_, _) => {
+        throw new Exception("unification error t1 = " + t1.toString + "t2 = " +  t2.toString)
+      }
+    }
+  }
+
+  def infer(e: Ast.Exp) = {
+    unify(Type.Unit(), g(Map.empty, e))
+  }
+}
